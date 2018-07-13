1109ef7ce3e2c95f698e0894926617bbe4ec1362
caifang <cooldish1245@163.com>
2018-07-13 17:34:58 +0800

    初始代码提交

---
+++ app/src/androidTest/java/wostore/infinit/com/testgitdemo0713/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package wostore.infinit.com.testgitdemo0713;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("wostore.infinit.com.testgitdemo0713", appContext.getPackageName());
+    }
+}
